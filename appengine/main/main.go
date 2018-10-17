587443c3470c95c067e522600b8d1d55e0d139ff
takemiyayusukeup <yusuke.takemiya@upstairs.jp>
2018-10-17 14:16:29 +0900

    とりあえず粗造で仮組

--- appengine/main/main.go
+++ appengine/main/main.go
@@ -12,9 +12,36 @@ import (
 
 func main() {
 	http.HandleFunc("/test", test)
+	http.HandleFunc("/ml", ml)
 	appengine.Main()
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "test!")
+}
+
+func ml(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ml!")
+
+	image = ""
+	projectid = "dogkimoti"
+	modelid = "ICN4592178324928500759"
+	url = "https://beta-dot-custom-vision.appspot.com/v1beta1/projects/" + projectid + "/locations/us-central1/models/" + modelid + ":predict "
+
+	params = 
+	payload : {
+		image: {
+			imageBytes: image
+		}
+	}
+
+	// jsonをデコードする
+	input, err := json.Marshal(params)
+
+	//params = {}
+	//request = prediction_client.predict(modelid, payload, params)
+	response, err := http.Post(url, "application/json", payload)
+
+	fmt.Fprintln(w, "response")
+	fmt.Fprintln(w, response)
 }
