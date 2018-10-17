c16c5c3ccede46fe650270c50712db91f80e5cb2
takemiyayusukeup <yusuke.takemiya@upstairs.jp>
2018-10-17 10:23:00 +0900

    rename main

--- appengine/view/main.go
+++ appengine/view/main.go
@@ -12,10 +12,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handle)
+	http.HandleFunc("/test", handle)
 	appengine.Main()
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+	fmt.Fprintln(w, "test!")
 }
